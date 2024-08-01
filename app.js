ceda985564e43dd004728a425fb333689f3710dd
guna.dermawan <guna.dermawan@prosia.co.id>
2024-08-01 12:06:23 +0700

    revert file to prepare demo internal

--- app.js
+++ app.js
@@ -87,6 +87,6 @@ if (app.get("env") == "development") {
 var token = "SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9";
 console.log("token: " + token);
 
-https.createServer(app).listen(app.get("port"), function () {
+http.createServer(app).listen(app.get("port"), function () {
   console.log("Express server listening on port " + app.get("port"));
 });
